568658ba70bc06200e8fb3984529abfbf75338a9
Viktoriia <toriakozenko@gmail.com>
2023-03-06 18:31:48 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+
+// 1
+
+// const week = 7;
+// const coffeeForDay = prompt("How much cups of coffee do you drink during the day??"); 
+// const coffeeForWeek = coffeeForDay * week; 
+// alert(`Your drink ${coffeeForWeek} cups of coffee for week`);
+
+// 2
+
+// const milkForBuy = prompt("How much milk do you buy during the month??");
+// const price = prompt("What is the cost of one milk?");
+// const spentMoney= milkForBuy * price; 
+// alert(`Your spend ${spentMoney} UAH`);
+
